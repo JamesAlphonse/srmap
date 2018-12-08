acf1d5b4518f649020a82de955bb552c822939fe
James <james@STG-05.local>
2018-12-07 21:32:20 -0500

    first commit with new code base

---
+++ src/App.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <div id="map-container" @mousemove="onMouseMove">
+      <simple-svg
+        :filepath="map.filepath"
+        :fill="map.fill"
+        :stroke="map.stroke"
+        :width="map.width"
+        :height="map.height"
+        :id="map.id"
+      />
+    </div>
+
+    <InfoContainer :package="info_data"/>
+  </div>
+</template>
+
+<style lang="scss">
+  body {
+    padding: 0; margin: 0;
+  }
+
+  #map-container {
+    position: relative;
+    display: block;
+    margin: 0px auto;
+    background: grey;
+  }
+
+  .state {
+    fill: #B0B0B0;
+    transition: all 0.2s;
+  }
+
+  .active {
+    fill: #89baf6;
+  }
+
+  .active:hover {
+    fill: #1547D1;
+  }
+</style>
+
+<script>
+  import {SimpleSVG} from 'vue-simple-svg'
+  import InfoContainer from '@/components/InfoContainer'
+
+  export default {
+    name: 'App',
+    components: {
+      'simple-svg': SimpleSVG,
+      InfoContainer
+    },
+    mounted() {
+    },
+    methods: {
+      onMouseMove(e) {
+        this.info_data = {
+          x: e.pageX,
+          y: e.pageY,
+          id: e.target.id,
+          class: e.target.className.baseVal,
+        }
+      }
+    },
+    data() {
+      return {
+        map: {
+          filepath: '/static/map.svg',
+          fill: '#d2d2d2',
+          stroke: '',
+          width: '100%',
+          height: '100%',
+          id: 'map'
+        },
+        info_data: {},
+      }
+    }
+  }
+
+</script>
---
+++ src/components/InfoContainer.vue
@@ -0,0 +1,98 @@
+<template>
+    <!--
+      :style="{transform: `translate3d(${point.x}px,${point.y}px,-1px)`, opacity: display}"
+    -->
+    <div id="info-container" ref="info"
+      :style="{
+        transform: `translate3d(${point.x}px,${point.y}px,-1px)`,
+        opacity: opacity
+      }"
+    >
+      {{ state.name }}
+    </div>
+</template>
+
+<style lang="scss" scoped>
+  #info-container {
+    position: absolute;
+    display: block;
+    width: 200px;
+    max-width: 200px;
+    min-height: 60px;
+    text-align: center;
+    line-height: 60px;
+    top: 0;
+    left: 0;
+    transform: translate(0, 0);
+    border: 1px solid #fff;
+    background: rgba(0,0,0,0.8);
+    transition: transform 0.07s, opacity 0.5s, content 2s;
+    will-change: transform;
+    color: #fff;
+    pointer-events: none;
+    box-shadow: 0px 0px 10px 1px #ccc;
+    opacity: 0;
+    z-index: 999;
+  }
+</style>
+
+<script>
+  import jsonRaw from '@/data/states.json'
+
+  export default {
+    name: 'InfoContainer',
+    props: ['package'],
+    components: {
+    },
+    mounted() {
+    },
+    watch: {
+      package: function() {
+        if(this.package.class == 'state active' && this.package.id.length == 2){
+          if(this.package.x < this.threshold)
+            this.offset.x = 50
+          else if(this.package.x > window.innerWidth - this.threshold)
+            this.offset.x = -50 - this.$refs.info.clientWidth
+
+          if(this.package.y < this.threshold)
+            this.offset.y = 50
+          else if(this.package.y > window.innerHeight - this.threshold)
+            this.offset.y = -50 - this.$refs.info.clientHeight
+
+          this.point.x = this.package.x + this.offset.x
+          this.point.y = this.package.y + this.offset.y
+          
+          this.opacity = 1
+
+          if(this.json[this.package.id])
+            this.state.name = this.json[this.package.id].name 
+        }
+        else{
+          this.opacity = 0
+          this.state.name = ''
+        }
+      }
+    },
+    methods: {
+    },
+    data() {
+      return {
+        json: jsonRaw,
+        state: {
+          name: ''
+        },
+        point: {
+          x: 0,
+          y: 0
+        },
+        threshold: 250,
+        offset: {
+          x: 50,
+          y: 50
+        },
+        opacity: 0
+      }
+    }
+  }
+
+</script>
